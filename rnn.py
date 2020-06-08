8a456df334688c25e30c30dc19b2277358c98c62
mathur15 <som.mathur1997@gmail.com>
2020-06-08 14:25:08 -0400

    Preprocessing and reshaped with one feature- google open price, need to figure out how to add second feature

--- rnn.py
+++ rnn.py
@@ -2,12 +2,48 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+#Note: All structures should be numpy arrays for rnn
 
 #import training set
+#nasdaq_dataset = pd.read_csv('NASDAQ_index.csv')
+#nasdaq_open = nasdaq_dataset['Open']
+#nasdaq_open_train = nasdaq_open.iloc[:1258]
 
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
-training_set = dataset_train.iloc[:,1:2].values
+#training_set_new = pd.concat([nasdaq_open_train,dataset_train['Open']],axis=1)
+training_set_new = dataset_train.iloc[:,1:2].values
 
 #instead of standardising, normalize for rnn
 scalar = MinMaxScaler()
-training_set_scaled = scalar.fit_transform(training_set)
+training_set_scaled = scalar.fit_transform(training_set_new)
+
+#trends based on 60 previous timestamps and 1 output(time t+1)
+#for each observation to predict 60 preious financial days will be used
+#y_train contain the actual price
+#each row in x_train correspond to the 60 previous timestamps for a given day
+x_train = []
+y_train = []
+#x_train_open = []
+#x_train_nasdaq = []
+for i in range(60,1258):
+    x_train.append(training_set_scaled[i-60:i,0])
+    #x_train_nasdaq.append(training_set_scaled[i,1])
+    y_train.append(training_set_scaled[i,0])
+#x_train.append([x_train_open])
+#x_train.append([x_train_nasdaq])
+#x_train_open = np.array(x_train_open)
+#x_train_nasdaq = np.array(x_train_nasdaq)
+#x_train_nasdaq = x_train_nasdaq.reshape(-1,1)
+#x_train = np.concatenate(x_train_open,x_train_nasdaq)
+x_train,y_train = np.array(x_train),np.array(y_train)
+
+#add more indicators- convert x_train to 3 dimensional for rnn layers
+#so far only one indicator---open stock price
+x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1],1))
+
+
+    
