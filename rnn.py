5892316341904ba06bebc56fb2863074419cfdc0
mathur15 <som.mathur1997@gmail.com>
2020-06-07 23:21:23 -0400

    Normalized data instead of standardizing

--- rnn.py
+++ rnn.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 #import training set
 
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
-training_set = np.array(dataset_train.iloc[:,1])
-#alternate way to convert to numpy arrays
-#training_set = dataset_train.iloc[:,1:2].values
+training_set = dataset_train.iloc[:,1:2].values
+
+#instead of standardising, normalize for rnn
+scalar = MinMaxScaler()
+training_set_scaled = scalar.fit_transform(training_set)
